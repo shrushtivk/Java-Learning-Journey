4f85fa0a2ad4e4909e53c3e27773a64d218511a7
Shrushti <302252822+shrushtivk@users.noreply.github.com>
2026-07-29 23:41:28 +0530

    Add StringIntroduction program

---
+++ StringIntroduction.java
@@ -0,0 +1,28 @@
+/*
+Question:
+Write a Java program to create and print a String.
+
+Concepts Covered:
+- String
+- Variables
+- Printing Output
+- System.out.println()
+
+Sample Output:
+Hello, Java!
+
+Explanation:
+This program creates a String variable and prints it on the screen.
+
+
+*/
+
+public class StringIntroduction {
+
+    public static void main(String[] args)
+  {
+
+          String message = "Hello, Java!";
+           System.out.println(message);
+  }
+}
