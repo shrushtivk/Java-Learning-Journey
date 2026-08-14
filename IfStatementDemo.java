70543c98ce8f878436a7f4c077f173898a8f4806
Shrushti <302252822+shrushtivk@users.noreply.github.com>
2026-08-14 22:10:58 +0530

    Add IfStatementDemo program

---
+++ IfStatementDemo.java
@@ -0,0 +1,37 @@
+/*
+Question:
+Write a Java program to demonstrate the if statement.
+Check whether a given number is greater than 10.
+
+Concepts Covered:
+- if statement
+- int Data Type
+- Variables
+- Comparison Operator (>)
+- System.out.println()
+
+Sample Input:
+15
+
+Sample Output:
+Number is greater than 10
+
+Explanation:
+The if statement is used to execute a block of code
+when a given condition is true.
+
+
+*/
+
+public class IfStatementDemo {
+
+    public static void main(String[] args) 
+  {
+     int num = 15;
+
+        if (num > 10) 
+        {
+           System.out.println("Number is greater than 10");
+     }
+ }
+}
